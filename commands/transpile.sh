0ef8b42b84f19542d4ab0594baa0663d5006cfb3
Giacomo Triggiano <giacomotriggiano@gmail.com>
2017-06-27 16:33:43 +0200

    fix

--- commands/transpile.sh
+++ commands/transpile.sh
@@ -7,7 +7,7 @@ source "${BASH_SOURCE%/*}/common.sh"
 echo
 echo -n "Transpiling package... "
 runAsService development "rm -rf lib/*" &> /dev/null
-runAsService development "babel src -d lib" &> /dev/null
+runAsService development "babel src -D -d lib" &> /dev/null
 echo "Done."
 echo -n "Transpiling tests... "
 runAsService development "rm -rf lib-tests/*" &> /dev/null
